a683296a9a191d48f55ce5aee030635a987e9bdc
Digitalvocals <gbuchanan21@gmail.com>
2026-01-06 22:51:22 -0800

    Fix: robots.txt sitemap URL to use www (SEO canonical)

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://streamscout.gg/sitemap.xml',
+    sitemap: 'https://www.streamscout.gg/sitemap.xml',
   }
 }
